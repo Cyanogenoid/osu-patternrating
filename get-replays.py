9d82f7762c47d5aa1389c310025fb4c18ba4baba
Yan Zhang <cyanogenoid@cyanogenoid.com>
2016-12-26 21:52:54 +0000

    Break rate limit when getting replays

--- get-replays.py
+++ get-replays.py
@@ -1,5 +1,4 @@
 import json
-import time
 import sys
 import os
 
@@ -9,18 +8,16 @@ from tqdm import tqdm
 import api
 
 
-REQUESTS_PER_MINUTE = 10
-
 with open('beatmap-scores.json') as fd:
     scores = json.loads(fd.read())
 
+session = requests.Session()
 url = "https://osu.ppy.sh/api/get_replay"
 for beatmap in tqdm(sorted(scores.keys())):
     beatmap_path = os.path.join('data', beatmap)
     if not os.path.exists(beatmap_path):
         os.makedirs(beatmap_path)
     for user in tqdm(sorted(scores[beatmap])):
-        start_time = time.perf_counter()
         path = os.path.join(beatmap_path, '{}.b64'.format(user))
         if os.path.exists(path):
             continue
@@ -32,17 +29,13 @@ for beatmap in tqdm(sorted(scores.keys())):
         success = False
         while not success:
             try:
-                r = requests.get(url, params=payload)
+                r = session.get(url, params=payload)
                 r.raise_for_status()
                 results = json.loads(r.text)
                 success = True
+                replay = results['content']
             except Exception as e:
-                print('ERROR ({}): {}'.format(user_id, e), file=sys.stderr)
-                time.sleep(60/REQUESTS_PER_MINUTE)
+                print('ERROR ({} on {}): {}'.format(user, beatmap, e), file=sys.stderr)
 
         with open(path, 'w') as fd:
-            fd.write(results['content'])
-        end_time = time.perf_counter()
-        time_diff = end_time - start_time
-        wait_time = max(0, 60/REQUESTS_PER_MINUTE - time_diff)
-        time.sleep(wait_time)
+            fd.write(replay)
