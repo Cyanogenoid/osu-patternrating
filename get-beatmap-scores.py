dff1c8c01565daa9ca89cccc98da5efcb2687cdc
Yan Zhang <cyanogenoid@cyanogenoid.com>
2016-12-26 21:52:26 +0000

    Include mod information in beatmap score list

--- get-beatmap-scores.py
+++ get-beatmap-scores.py
@@ -10,8 +10,9 @@ import api
 with open('beatmaps.csv') as fd:
     beatmaps = fd.read().split(',')
 
-beatmap_scores = {}
+beatmap_mods = {}
 
+session = requests.Session()
 url = "https://osu.ppy.sh/api/get_scores"
 for i, beatmap in enumerate(tqdm(beatmaps)):
     payload = {
@@ -21,7 +22,7 @@ for i, beatmap in enumerate(tqdm(beatmaps)):
     success = False
     while not success:
         try:
-            r = requests.get(url, params=payload)
+            r = session.get(url, params=payload)
             r.raise_for_status()
             results = json.loads(r.text)
             success = True
@@ -29,7 +30,7 @@ for i, beatmap in enumerate(tqdm(beatmaps)):
             print('ERROR ({}): {}'.format(beatmap, e), file=sys.stderr)
 
     for entry in results:
-        beatmap_scores[beatmap] = [entry['user_id'] for entry in results]
-
+        beatmap_mods.setdefault(beatmap, {})[entry['user_id']] = entry['enabled_mods']
+         
 with open('beatmap-scores.json', 'w') as fd:
     fd.write(json.dumps(beatmap_scores))
