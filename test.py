fb0d60ebb8a8be86cd35d391725724b8d8475599
Yan Zhang <cyanogenoid@cyanogenoid.com>
2017-01-07 23:43:11 +0000

    Make test script work and run a few sanity tests

--- test.py
+++ test.py
@@ -3,19 +3,48 @@ from keras.models import load_model
 
 
 model = load_model('test.model')
+model.summary()
 
 
-mean = [3.68264337e+02, 2.25967367e-01, 8.64548766e-02, 1.15329634e+01]
-std = [565.7039326, 116.71451414, 104.41504112, 1.67337289]
+with open('.train-stats') as fd:
+    m0, m1, std0, std1 = map(float, fd.read().split(','))
 
 bpm = 180
 rhythm = 1/2
-tdelta = [60 * 1000 / bpm * rhythm for _ in range(3)]
+tdelta = np.array([60 * 1000 / bpm * rhythm for _ in range(2)])
+tdelta = (tdelta - m0) / std0
 
-def evaluate(xdelta, ydelta):
-    data = np.array([tdelta, xdelta, ydelta], dtype=np.float).T
-    data = (data - mean[:3]) / std[:3]
-
-    data = data.reshape(1, *data.shape)
+def evaluate(first_length, angle_between, second_length):
+    '''
+    first_length: distance of first movement in pixels
+    angle_between: angle between the two movements,
+                   between -0.5 (0 degrees) and 0.5 (180 degrees)
+    second_length: distance of second movement in pixels
+    '''
+    lengths = (np.array([first_length, second_length]) - m1) / std1
+    data = np.concatenate([tdelta, lengths, [angle_between]])
+    data = data.reshape(1, data.shape[0])
     prediction = model.predict(data)
     print('difficulty score:', prediction)
+
+
+print('\n### pattern difficulty ###')
+
+print('back and forth')
+evaluate(200, 0.5, 200)
+print('very acute triangle')
+evaluate(200, 0.375, 200)
+print('acute triangle')
+evaluate(200, 0.25, 200)
+print('slightly acute triangle')
+evaluate(200, 0.125, 200)
+print('square')
+evaluate(200, 0, 200)
+print('slightly obtuse triangle')
+evaluate(200, -0.125, 200)
+print('obtuse triangle')
+evaluate(200, -0.25, 200)
+print('very obtuse triangle')
+evaluate(200, -0.375, 200)
+print('straight line')
+evaluate(200, -0.5, 200)
