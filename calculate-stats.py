4f133d227cd288fb97459470945b0e6869713cbd
Yan Zhang <cyanogenoid@cyanogenoid.com>
2017-01-08 10:32:01 +0000

    Treat replays with mods differently, include CS

--- calculate-stats.py
+++ calculate-stats.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 import numpy as np
 import h5py
@@ -19,40 +20,55 @@ def parse_replay(s):
 def process_beatmap(path):
     with open(path) as fd:
         cs, *beatmap = next(fd).split(',')
+        cs = float(cs)
         beatmap = parse_beatmap(beatmap)
         lines = fd.read().splitlines()
 
-    replays = np.zeros([len(lines), beatmap.shape[0], 2])
+    mod = defaultdict(list)
     for i, line in enumerate(lines):
-        # TODO treat hr and dt mod combinations differently
-        # for now, just pretend they're all nomod...
         hr, dt, *replay = line.split(',')
-        replays[i, :, :] = parse_replay(replay)
+        hr, dt = int(hr), int(dt)
+        mod[hr, dt].append(parse_replay(replay))
 
+    for mod_combination in list(mod.keys()):
+        mod[mod_combination] = np.array(mod[mod_combination])
+
+    # beatmap info
     movement = np.diff(beatmap, axis=0)
     times_delta = movement[:, 0]
     movement_lengths = np.linalg.norm(movement[:, 1:], axis=1)
     movement_angles = np.arctan2(movement[:, 2], movement[:, 1])
-    mean = np.nanmean(replays, axis=0)
-    delta = beatmap[:, 1:] - mean
-    stdev = np.mean(np.nanstd(replays, axis=0), axis=1)
 
-    # first hitobject doesn't have any movement towards it, so it's ignored
-    # data thus has to start with the second hitobject
-    data = np.zeros([movement.shape[0], movement.shape[1] + 1])
-    data[:, 0] = times_delta
-    data[:, 1] = movement_lengths
-    data[:, 2] = movement_angles
-    data[:, 3] = stdev[1:]
-    data[:, 4] = cs
-    # TODO add mean difference if it seems useful
-    # TODO include correlation information
-    # TODO beatmap difficulty metadata
-    return data
+    modded_beatmaps = []
+    for mod_combination, replays in mod.items():
+        if replays.shape[0] < 10:
+            # too few replays to get anything useful out of it
+            continue
+        # replay info
+        mean = np.nanmean(replays, axis=0)
+        delta = beatmap[:, 1:] - mean
+        stdev = np.mean(np.nanstd(replays, axis=0), axis=1)
+
+        rate_multiplier = 1.5 if dt > 0 else 0.75 if dt < 0 else 1.0
+        diff_multiplier = 1.4 if hr > 0 else 0.5 if hr < 0 else 1.0
+        # first hitobject doesn't have any movement towards it, so it's ignored
+        # data thus has to start with the second hitobject
+        data = np.zeros([movement.shape[0], movement.shape[1] + 2])
+        data[:, 0] = times_delta / rate_multiplier
+        data[:, 1] = movement_lengths
+        data[:, 2] = movement_angles
+        data[:, 3] = stdev[1:]
+        data[:, 4] = cs * diff_multiplier
+        # TODO mean difference if it seems useful
+        # TODO correlation/covariance instead of mean of variance
+        # TODO more beatmap difficulty metadata
+        modded_beatmaps.append(data)
+    return modded_beatmaps
 
 
 file = h5py.File('data.hdf5')
 for beatmap in os.listdir('data'):
     if beatmap.endswith('.csv'):
         path = os.path.join('data', beatmap)
-        file[beatmap[:-4]]= process_beatmap(path)
+        for i, modded_beatmap in enumerate(process_beatmap(path)):
+            file['{}-{}'.format(beatmap[:-4], i)] = modded_beatmap
